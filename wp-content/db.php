634a454ed5e7c56225d9a207ef76ab9797216a7d
Rheinard Korf <rheinard@thekorfs.com>
2021-01-30 13:59:12 +1100

    messy

--- wp-content/db.php
+++ wp-content/db.php
@@ -13,10 +13,32 @@ class DynoPressDriver extends wpdb {
 	/**
 	 * Default construct.
 	 */
-	public function __construct() {
-		parent::__construct( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST );
+	public function __construct( $u, $p, $db, $host ) {
+		parent::__construct( $u, $p, $db, $host );
+		// parent::__construct( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST );
+	}
+
+	/**
+	 * Everything hinges here!
+	 *
+	 * @param string $query DB query.
+	 * @return void
+	 */
+	public function query( $query ) {
+		// die( $query );
+		if ( "SELECT option_name, option_value FROM wp_options WHERE autoload = 'yes'" === $query ) {
+			$this->last_result = $this->fake_options();
+			$this->num_rows    = 116;
+			return 116;
+		}
+		$result = parent::query( $query );
+		return $result;
+	}
+
+	private function fake_options() {
+		return;
 	}
 }
 
 global $wpdb;
-$wpdb = new DynoPressDriver();
+$wpdb = new DynoPressDriver( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST );
